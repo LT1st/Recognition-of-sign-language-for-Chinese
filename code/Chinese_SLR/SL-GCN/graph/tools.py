00a735f737a511b0cd707ccbeead0aeca74e33c4
yofchio666 <1625376106@qq.com>
2023-03-04 12:05:40 +0800

    delete dataset

---
+++ code/Chinese_SLR/SL-GCN/graph/tools.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+#返回矩阵图
+def edge2mat(link, num_node):
+    A = np.zeros((num_node, num_node))
+    for i, j in link:
+        A[j, i] = 1
+    return A
+
+# 归一化
+def normalize_digraph(A):  # 除以每列的和
+    Dl = np.sum(A, 0)
+    h, w = A.shape
+    Dn = np.zeros((w, w))
+    for i in range(w):
+        if Dl[i] > 0:
+            Dn[i, i] = Dl[i] ** (-1)
+    AD = np.dot(A, Dn)
+    return AD
+
+
+def get_spatial_graph(num_node, self_link, inward, outward):
+    I = edge2mat(self_link, num_node)
+    In = normalize_digraph(edge2mat(inward, num_node))
+    Out = normalize_digraph(edge2mat(outward, num_node))
+    A = np.stack((I, In, Out)) # 堆叠函数， 按行
+    return A
